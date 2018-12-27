99fc5953e2c97ae6545d79cefcccd2d2d64cf692
Michael Gagliardo <mgagliardo91@gmail.com>
2018-12-27 15:14:09 -0600

    Initial commit

---
+++ blacksmith.go
@@ -0,0 +1,160 @@
+package blacksmith
+
+import (
+	"github.com/mgagliardo91/go-utils"
+)
+
+// TaskName is an enumeration of jobs to execute
+type TaskName int
+
+// Task represents a unit of work to execute
+type Task struct {
+	LogProvider
+	// Payload holds the "body" of the task
+	Payload interface{} `json:"payload"`
+	// TaskName identifies the type of task
+	TaskName TaskName `json:"taskName"`
+}
+
+// TaskHandler is a function used to execute a type of task
+type TaskHandler func(task Task)
+
+// Worker works on a goroutine to carry out a Task
+type Worker struct {
+	LogProvider
+	workerPool  chan chan Task
+	taskChannel chan Task
+	stopChannel utils.StopChannel
+	executeTask TaskHandler
+}
+
+// Blacksmith is in charge of splitting and dispatching work to workers
+type Blacksmith struct {
+	LogProvider
+	taskQueue   chan Task
+	workerPool  chan chan Task
+	maxWorkers  int
+	stopChannel utils.StopChannel
+	workers     []Worker
+	handlerMap  map[TaskName]TaskHandler
+	handlerFn   TaskHandler
+}
+
+// New generates a Blacksmith in charge of creating/dividing tasks
+func New(maxWorkers int) *Blacksmith {
+	blacksmith := Blacksmith{
+		taskQueue:   make(chan Task),
+		workerPool:  make(chan chan Task, maxWorkers),
+		maxWorkers:  maxWorkers,
+		stopChannel: utils.NewStopChannel(),
+		workers:     make([]Worker, maxWorkers),
+		handlerMap:  make(map[TaskName]TaskHandler),
+	}
+	blacksmith.InitLog("Blacksmith")
+	return &blacksmith
+}
+
+// SetHandlerFn sets a TaskHandler that allows the client to handle any task
+func (b *Blacksmith) SetHandlerFn(taskHandler TaskHandler) *Blacksmith {
+	b.handlerFn = taskHandler
+	return b
+}
+
+// SetHandler maps a task name to a task handler so that all tasks of this name will be executed by the handler
+func (b *Blacksmith) SetHandler(taskName TaskName, taskHandler TaskHandler) *Blacksmith {
+	b.handlerMap[taskName] = taskHandler
+	return b
+}
+
+// Run starts the Blacksmith which will initialize the workers
+func (b *Blacksmith) Run() *Blacksmith {
+	b.Logf("Starting %v workers\n", b.maxWorkers)
+	for i := 0; i < b.maxWorkers; i++ {
+		worker := Worker{
+			workerPool:  b.workerPool,
+			taskChannel: make(chan Task),
+			stopChannel: utils.NewStopChannel(),
+		}
+		worker.InitLog("worker").SetPrefix(b.Identifier())
+		worker.start(b.executeTask)
+		b.workers[i] = worker
+	}
+
+	go b.dispatch()
+
+	return b
+}
+
+// Stop shuts down the Blacksmith which will wait for all workers to complete
+func (b *Blacksmith) Stop() *Blacksmith {
+	b.Log("Received request to stop")
+	b.stopChannel.RequestStop()
+	return b
+}
+
+// QueueTask adds a new task to the work queue
+func (b *Blacksmith) QueueTask(taskName TaskName, payload interface{}) {
+	b.taskQueue <- Task{TaskName: taskName, Payload: payload}
+}
+
+func (b *Blacksmith) dispatch() {
+	for {
+		select {
+		case job := <-b.taskQueue:
+			go func(job Task) {
+				taskChannel := <-b.workerPool
+
+				b.Logf("Dispatching Task: %+v\n", job)
+				taskChannel <- job
+			}(job)
+		case <-b.stopChannel.OnRequest:
+			go func() {
+				b.Log("Closing all workers")
+				for _, worker := range b.workers {
+					worker.stop()
+				}
+				b.Log("Quitting")
+				b.stopChannel.Stop()
+			}()
+		}
+	}
+}
+
+func (b *Blacksmith) executeTask(task Task) {
+	if b.handlerFn != nil {
+		b.handlerFn(task)
+		return
+	}
+
+	t := b.handlerMap[task.TaskName]
+
+	if t != nil {
+		t(task)
+	} else {
+		b.Logf("Cannot locate task handler for task name: %s", task.TaskName)
+	}
+}
+
+func (worker Worker) start(taskHandler TaskHandler) {
+	worker.Log("Started")
+	go func() {
+		for {
+			worker.workerPool <- worker.taskChannel
+
+			select {
+			case task := <-worker.taskChannel:
+				task.InitLog("Task").SetPrefix(worker.Identifier())
+				worker.Logf("Processing task %+v\n", task)
+				taskHandler(task)
+			case <-worker.stopChannel.OnRequest:
+				worker.Log("Quitting")
+				worker.stopChannel.Stop()
+				return
+			}
+		}
+	}()
+}
+
+func (worker Worker) stop() {
+	worker.stopChannel.RequestStop()
+}
---
+++ logger.go
@@ -0,0 +1,88 @@
+package blacksmith
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+)
+
+// LogFormatFn overloads the logging log.fmt(string, format)
+type LogFormatFn func(string, ...interface{})
+
+// LogFn overloads the standard log.xx()
+type LogFn func(...interface{})
+
+// LogProvider represents an entity that can log. All logged statements will be formatted
+// with the identity of the LogProvider
+type LogProvider struct {
+	id     uuid.UUID
+	name   string
+	prefix string
+}
+
+// InitLog initializes the LogProvider
+func (lP *LogProvider) InitLog(name string) *LogProvider {
+	lP.name = name
+	lP.id = uuid.New()
+
+	return lP
+}
+
+// SetPrefix passes a parent identity that will be logged at the start of each log statement
+func (lP *LogProvider) SetPrefix(prefix string) *LogProvider {
+	lP.prefix = prefix
+	return lP
+}
+
+// Identifier returns a A-ID|B-ID|C-ID pattern for the LogProvider
+func (lP LogProvider) Identifier() string {
+	identifier := lP._identifier()
+
+	if lP.prefix != "" {
+		identifier = fmt.Sprintf("%s|%s", lP.prefix, identifier)
+	}
+
+	return identifier
+}
+
+func (lP LogProvider) _identifier() string {
+	return fmt.Sprintf("%s-%s", lP.name, lP.id)
+}
+
+func (lP LogProvider) buildLogPrefix() string {
+	logStatement := fmt.Sprintf("[%s]:", lP._identifier())
+
+	if lP.prefix != "" {
+		logStatement = fmt.Sprintf("(%s) %s", lP.prefix, logStatement)
+	}
+
+	return logStatement
+}
+
+// Logf prints the value and arguments using the standard log.Printf
+func (lP LogProvider) Logf(value string, args ...interface{}) {
+	lP.LogfUsing(log.Printf, value, args...)
+}
+
+// LogfUsing prints the value and arguments using the provided LogFormatFn
+func (lP LogProvider) LogfUsing(logFn LogFormatFn, value string, args ...interface{}) {
+	logStatement := fmt.Sprintf("%s %s", lP.buildLogPrefix(), value)
+	logFn(logStatement, args)
+}
+
+// Log prints the value and arguments using the standard log.Println
+func (lP LogProvider) Log(value string) {
+	lP.LogUsing(log.Println, value)
+}
+
+// LogUsing prints the value and arguments using the provided LogFn
+func (lP LogProvider) LogUsing(logFn LogFn, value string, args ...interface{}) {
+	logStatement := fmt.Sprintf("%s %s", lP.buildLogPrefix(), value)
+
+	if len(args) > 0 {
+		logFn(logStatement, args)
+	} else {
+		logFn(logStatement)
+	}
+}
